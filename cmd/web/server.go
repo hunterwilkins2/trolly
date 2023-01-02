a8320b1eced1e3b2224d2e52a764452134f7aac6
Hunter Wilkins <hunter.wilkins2@gmail.com>
2023-01-02 13:21:58 -0500

    Added ca-certificates

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"net/http"
 	"time"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func (app *application) serve() error {
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache("certs"),
+		HostPolicy: autocert.HostWhitelist("hunterwilkins.dev"),
+	}
+
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
 	}
 
 	app.logger.Printf("starting %s server on port :%d", app.config.env, app.config.port)
-	err := srv.ListenAndServe()
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	err := srv.ListenAndServeTLS("", "")
 	if !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -12,3 +12,8 @@ require (
 	github.com/justinas/nosurf v1.1.1
 	golang.org/x/crypto v0.4.0
 )
+
+require (
+	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
